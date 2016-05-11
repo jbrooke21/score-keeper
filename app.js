63036e97b311989603a9544ee1e9a8fee6f4edf9
jbrooke21 <jessica.brooke@symphony.com>
2016-05-10 19:45:26 -0700

    updates

---
+++ app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+		var score = 0;
+	
+		
+
+	$(document).on("click", "#increase-5", function() {
+		// alert("works!");
+		score = score + 5;
+		$("#score").html(score + " points");
+	});
+
+	$(document).on("click", "#decrease-5", function() {
+		// alert("works!");
+
+		if (score - 5 >= 0) {
+		
+			score = score - 5;
+			// score + = 5;
+			$("#score").html(score + " points");
+		}
+	});
+
+	$(document).on("click", "#submit-custom-score", function() {
+		score = parseInt($("#custom-score").val());
+		$("#score").html(score + "points!");
+	});
+
+
+});
